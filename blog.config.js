cd281ed8b39dfbaa436502c7f250fa2c5124b0c4
PonderingParticles <155112157+PonderingParticles@users.noreply.github.com>
2024-08-02 01:16:42 -0400

    Update blog.config.js
    adding notion page id

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,8 @@ const BLOG = {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  //notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
+  notionPageId: 129fd5b77ecb40e980492f8baf356bc3
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
     provider: '', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
